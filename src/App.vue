74a9a57656f13660beb36a69c0a8d2c428809468
bei <liub@docimax.com.cn>
2019-11-26 11:34:30 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <div id="app"></div>
 </template>
+
+<script>
+import error from "./errorpage";
+export default {
+  name: "app",
+  mounted() {
+    error({
+      msg: "错误提示",
+      curl: "curl",
+      origin: "errorpage",
+      env: "dev",
+      aurl: "aurl",
+      clientid: "clientid"
+    });
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  font-family: sans-serif;
 }
 </style>
---
+++ src/errorpage/index.js
@@ -0,0 +1,31 @@
+import pkg from "../../package.json";
+export default function({ msg, curl, origin, env, aurl, clientid }) {
+  // eslint-disable-next-line no-console
+  console.log(`@docimax/ui version: ${pkg.version}`);
+
+  let template = `
+    <h3>太糟糕了!</h3>
+    <pre>
+应用程序的初始化遇到了问题! 但是我们捕捉到一个错误提示: 
+
+  <b>${msg}</b>
+
+通常来说, 诸如 Network Error 此类问题大多是由于没有从配置服务器请求到应用初始化所必须的配置所导致的.
+
+但是还请先确认配置信息是否正确：
+
+<b>配置服务</b>
+- url: ${curl}
+- key: ${origin}
+- env: ${env}
+
+<b>认证服务</b>
+- url: ${aurl ? aurl : "尚未获取"}
+- clientid: ${clientid ? clientid : "尚未获取"}
+
+另外, 你也可以按 F12 分析抛出的异常.
+
+希望这可以帮到你 :)</pre>`;
+
+  document.writeln(template);
+}
